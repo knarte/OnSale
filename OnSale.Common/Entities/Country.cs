89c45461a5e89554638fb40bae0903799bb49276
ccanarte <ccanarte@navisaf.com>
2020-10-19 15:09:53 -0500

    Add department to a country

--- OnSale.Common/Entities/Country.cs
+++ OnSale.Common/Entities/Country.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace OnSale.Common.Entities
 {
@@ -9,8 +8,13 @@ namespace OnSale.Common.Entities
     {
         public int Id { get; set; }
 
-        [MaxLength(50,ErrorMessage ="The field {0} must contain less than {1} characters")]
+        [MaxLength(50, ErrorMessage = "The field {0} must contain less than {1} characters")]
         [Required]
         public string Name { get; set; }
+        public ICollection<Department> Departments { get; set; }
+
+        [DisplayName("Departments Number")]
+        public int DepartmentsNumber => Departments == null ? 0 : Departments.Count;
+
     }
 }
